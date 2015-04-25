5549a795c8bb37216d4607f29a48b706c446eaf1
quillford <quillford@users.noreply.github.com>
2015-04-24 23:49:09 -0700

    XYZ Jogging

--- src/app.js
+++ src/app.js
@@ -3,8 +3,10 @@
 var UI = require("ui");
 var Vector2 = require("vector2");
 var Settings = require("settings");
+var ajax = require("ajax");
 
-var crrentAxis;
+var smoothieIP = "192.168.1.120"
+var currentAxis;
 var joggingIncrements = ["0.1", "1", "10", "100"];
 var joggingIncrement = joggingIncrements[1];
 
@@ -40,8 +42,21 @@ var jogging_increment = new UI.Text({
   font: 'gothic-18-bold',
   text: "Increment: " + joggingIncrement
 });
+var jogging_negative = new UI.Text({
+  position: new Vector2(115, 115),
+  size: new Vector2(144, 168),
+  font: 'gothic-18-bold'
+});
+var jogging_positive = new UI.Text({
+  position: new Vector2(115, 5),
+  size: new Vector2(144, 168),
+  font: 'gothic-18-bold'
+});
+
 jogging_window.add(jogging_axis);
 jogging_window.add(jogging_increment);
+jogging_window.add(jogging_negative);
+jogging_window.add(jogging_positive);
 
 main_menu.show();
 
@@ -68,9 +83,11 @@ main_menu.on("select", function(e) {
 });
 
 function switchAxis(axis){
-  crrentAxis = axis;
+  currentAxis = axis;
   jogging_axis.text(axis + " Axis");
   jogging_window.show();
+  jogging_negative.text("-"+axis);
+  jogging_positive.text("+"+axis)
 }
 
 jogging_window.on("click", "select", function(){
@@ -80,3 +97,37 @@ jogging_window.on("click", "select", function(){
   }
   jogging_increment.text("Increment: " + joggingIncrements[joggingIncrement].toString());
 });
+
+jogging_window.on("click", "up", function(){
+  var feedrate = 3000;
+  if(currentAxis == "Z"){
+    feedrate = 200; 
+  }
+  send_command("G91 G0 " + currentAxis + joggingIncrements[joggingIncrement] + " F" + feedrate+ " G90");
+});
+
+jogging_window.on("click", "down", function(){
+  var feedrate = 3000;
+  if(currentAxis == "Z"){
+    feedrate = 200; 
+  }
+  send_command("G91 G0 " + currentAxis + -(joggingIncrements[joggingIncrement]) + " F" + feedrate+ " G90");
+});
+
+function send_command(command){
+  console.log("sending " + command);
+  ajax(
+  {
+    url: "http://" + smoothieIP + "/command",
+    method: "post",
+    type: "text",
+    data: command + "\n"
+  },
+  function(data, status, request) {
+    console.log("Successfully sent command");
+  },
+  function(error, status, request) {
+    console.log("Request to smoothie failed");
+  }
+);
+}
